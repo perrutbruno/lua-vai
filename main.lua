b387a803ff63c5bafeebcfaa232dcce64c2961c4
fabriciorby <fabriciorby@hotmail.com>
2023-09-16 18:07:37 +0200

    commit for pull

--- main.lua
+++ main.lua
@@ -1,9 +1,10 @@
 package.path = package.path .. ";packages/?.lua"
 
 local json = require("json")
+local variables = {}
 
 function interpret(node)
-    local file = io.open("files/print.json", "r")
+    local file = io.open("files/let.json", "r")
     if file then
         local ast = file:read("*a")
         file:close()
@@ -35,28 +36,39 @@ function parse(ast)
 
     if ast.kind == "Call" then
         -- TODO checar se é sempre "Var"
-        variables[ast.callee.text](ast.arguments[0])
+        variables[ast.callee.text](ast.arguments[1].value)
     end
 
     if ast.kind == "Int" then
         return ast.value
     end
 
+    if ast.kind == "Function" then
+        for k, v in ipairs(ast.parameters) do
+            table.insert(variables, v.text)
+        end
+        return function()
+            return parse(ast.value)
+        end
+    end
+
+    if ast.kind == "Var" then
+        return variables[ast.text]
+    end
+
 end
 
-local variables = {}
-
--- function dump(o)
---     if type(o) == 'table' then
---        local s = '{ '
---        for k,v in pairs(o) do
---           if type(k) ~= 'number' then k = '"'..k..'"' end
---           s = s .. '['..k..'] = ' .. dump(v) .. ','
---        end
---        return s .. '} '
---     else
---        return tostring(o)
---     end
---  end
+function dump(o)
+    if type(o) == 'table' then
+       local s = '{ '
+       for k,v in pairs(o) do
+          if type(k) ~= 'number' then k = '"'..k..'"' end
+          s = s .. '['..k..'] = ' .. dump(v) .. ','
+       end
+       return s .. '} '
+    else
+       return tostring(o)
+    end
+ end
 
 interpret(1)
