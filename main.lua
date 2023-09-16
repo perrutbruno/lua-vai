b7e24f10da1e8a8baf48bc5adecaba6be7fcc50c
Bruno Muniz Perrut <bruno.muniz.perrut@gmail.com>
2023-09-16 15:39:10 +0200

    add of basic json reading

--- main.lua
+++ main.lua
@@ -4,4 +4,18 @@ local json = require("json")
 
 function interpret(node)
     local file = io.open("files/fib.json", "r")
+    if file then
+        local ast = file:read("*a") --read the whole file
+        -- close file after reading it and keeping its value in memory
+        file:close()
+
+        --saving it as a lua table so we can retrieve its values
+        local ast_data = json.decode(ast)
+
+        print(ast_data)
+    else
+        error("Could not find the AST file")
+    end
 end
+
+interpret(1)
