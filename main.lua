a8a3b30d10c9e7310911b59d19bfbba12dda3b7b
fabriciorby <fabriciorby@hotmail.com>
2023-09-16 16:33:10 +0200

    First recursive print

--- main.lua
+++ main.lua
@@ -3,19 +3,43 @@ package.path = package.path .. ";packages/?.lua"
 local json = require("json")
 
 function interpret(node)
-    local file = io.open("files/fib.json", "r")
+    local file = io.open("files/print.json", "r")
     if file then
-        local ast = file:read("*a") --read the whole file
-        -- close file after reading it and keeping its value in memory
+        local ast = file:read("*a")
         file:close()
 
         --saving it as a lua table so we can retrieve its values
         local ast_data = json.decode(ast)
 
-        print(ast_data)
+        parse(ast_data.expression)
     else
         error("Could not find the AST file")
     end
 end
+
+function parse(ast)
+
+    if ast.kind == "Print" then
+        print(parse(ast.value))
+    end
+
+    if ast.kind == "Str" then
+        return ast.value
+    end
+
+end
+
+-- function dump(o)
+--     if type(o) == 'table' then
+--        local s = '{ '
+--        for k,v in pairs(o) do
+--           if type(k) ~= 'number' then k = '"'..k..'"' end
+--           s = s .. '['..k..'] = ' .. dump(v) .. ','
+--        end
+--        return s .. '} '
+--     else
+--        return tostring(o)
+--     end
+--  end
 
 interpret(1)
